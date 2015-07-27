382f66b1411f07ac65feec2951bcb8720de86963
Fernando Angarita <fangari@gmail.com>
2015-07-27 13:30:20 -0500

    Add helper to make nav link active if it's current page.

--- helpers/localization_helpers.rb
+++ helpers/localization_helpers.rb
@@ -1,6 +1,6 @@
 module LocalizationHelpers
   def local_path(url, options = {})
-    lang = options.fetch(:lang, I18n.locale.to_s )
+    lang = options.fetch(:lang, I18n.locale.to_s)
     local_url = I18n.t("paths.#{url}")
     "/#{lang}/#{local_url}"
   end
@@ -11,5 +11,14 @@ module LocalizationHelpers
     else
       link_to(I18n.t(args.shift), local_path(args.shift, {}), args.shift)
     end
+  end
+
+  def nav_link_to(link, url, options = {})
+    if current_resource.url == (url_for(local_path(url)) + '/')
+      options.merge!(class: 'active') do |_key, oldval, newval| 
+        oldval +" "+ newval
+      end
+    end
+    localized_link_to(link, url, options)
   end
 end
